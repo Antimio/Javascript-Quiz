2b2ad08cedecd930e4e61144a63e9fd839cea5e0
Antimio <antimiocasimiro@gmail.com>
2024-01-16 23:52:51 +0000

    added the questions and answers to the used in the quiz

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,102 @@
+//Questions for the quiz
+var questions = [{
+        question: "Javascript is an _______ language?",
+        answers: [
+            "Object-orineted",
+            "Object-based",
+            "Procedural",
+            "None of the above"
+        ],
+        correctAnswer: "Object-orineted"
+    },
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        answers: [
+            "var",
+            "let",
+            "Both A and B",
+            "None of the above"
+        ],
+        correctAnswer: "Both A and B"
+    },
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        answers: [
+            "getElementbyID()",
+            "getElementsByClassName()",
+            "Both A and B",
+            "None of the above"
+        ],
+        correctAnswer: "Both A and B"
+    },
+    {
+        question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        answers: [
+            "Throws an error",
+            "Ignores the statements",
+            "Gives a warning",
+            "None of the above"
+        ],
+        correctAnswer: "Ignores the statements"
+    },
+    {
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        answers: [
+            "document.write()",
+            "console.log()",
+            "window.alert()",
+            "All of the above"
+        ],
+        correctAnswer: "All of the above"
+    },
+    {
+        question: "How can a datatype be declared to be a constant type?",
+        answers: [
+            "const",
+            "var",
+            "let",
+            "constant"
+        ],
+        correctAnswer: "const"
+    },
+    {
+        question: "What keyword is used to check whether a given property is valid or not?",
+        answers: [
+            "in",
+            "is in",
+            "exists",
+            "lies"
+        ],
+        correctAnswer: "in"
+    },
+    {
+        question: "What does HTML stand for?",
+        answers: [
+            "Hyperlinks and Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyper Text Markup Language",
+            "None of these"
+        ],
+        correctAnswer: "Hyper Text Markup Language"
+    },
+    {
+        question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        answers: [
+            "Boolean",
+            "Undefined",
+            "Object",
+            "Integer"
+        ],
+        correctAnswer: "Object"
+    },
+    {
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
+        answers: [
+            "stringify()",
+            "parse()",
+            "convert()",
+            "None of the above"
+        ],
+        correctAnswer: "stringify()"
+    }
+];
